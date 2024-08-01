52277459d7f96f31eb4792307b6b0b6208100c44
Luan <luandhx@gmail.com>
2024-07-31 22:26:11 -0300

    Update

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
-
 var SPEED = 300.0
+var rng = RandomNumberGenerator.new()
+
+func reset():
+	var randx = rng.randf_range(-200, 200)
+	var randY = rng.randf_range(150,450)
+	position = Vector2(400, randY)
+	velocity = Vector2(200, 200).normalized() * SPEED
 
 func _ready():
 	velocity = Vector2(200, 200).normalized() * SPEED
@@ -10,4 +16,4 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
-		SPEED += 1
+		SPEED += 2
--- scripts/score.gd
+++ scripts/score.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var ball = $"../Ball"
 @onready var p1_label = $"Player1 Score/Label"
 @onready var p2_label = $"Player2 Score/Label"
 var P1_SCORE = Game.player1_score
@@ -8,9 +9,11 @@ var P2_SCORE = Game.player2_score
 func _on_player_1_score_body_entered(body):
 	P1_SCORE += 1
 	p1_label.text = str(P1_SCORE)
+	ball.reset()
 
 func _on_player_2_score_body_entered(body):
 	P2_SCORE += 1
 	p2_label.text = str(P2_SCORE)
+	ball.reset()
 
 
