eff9e72d532d82b106ae57af3a4b347af3b29947
Luan <luandhx@gmail.com>
2024-08-03 18:47:25 -0300

    Add Return to main menu with Esc

--- scripts/multiplayer_game.gd
+++ scripts/multiplayer_game.gd
@@ -6,3 +6,8 @@ var player2_score = 0
 
 func _on_ball_timer_timeout():
 	$Ball.reset_ball()
+
+func _physics_process(delta):
+	var exit = Input.get_action_strength("exit_game")
+	if exit == 1:
+		get_tree().change_scene_to_file("res://main.tscn")
--- scripts/single_player_game.gd
+++ scripts/single_player_game.gd
@@ -6,3 +6,8 @@ var CPU_score = 0
 
 func _on_ball_timer_timeout():
 	$Ball.reset_ball()
+
+func _physics_process(delta):
+	var exit = Input.get_action_strength("exit_game")
+	if exit:
+		get_tree().change_scene_to_file("res://main.tscn")
